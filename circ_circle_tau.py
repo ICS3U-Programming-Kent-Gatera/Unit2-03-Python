cfe1e150a65ddff274f1c7d04a2c01855ba0906a
Kent Gatera <kent.gatera@stu.ocsb.ca>
2022-09-28 21:54:34 +0000

    calculates circumference of a circle with user input.

---
+++ circ_circle_tau.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Created By: Kent Gatera
+# Date: 09.28.22 circumference of a circle with user input.
+
+
+import math
+
+TAU = 6.28
+radius = input("What is the radius? ")
+float(radius)
+print("The circumference is {} cm".format(float(radius) * TAU))
